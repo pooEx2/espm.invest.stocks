6cfb13980468fa154a62e408f7db433a567cab8f
vitor-tk <62676267+Vitor-TK@users.noreply.github.com>
2021-06-11 16:46:51 -0300

    alteração no StocksService

--- src/main/java/br/espm/poo1/stocks/StocksService.java
+++ src/main/java/br/espm/poo1/stocks/StocksService.java
@@ -4,26 +4,22 @@ import br.espm.poo1.stocks.common.datatype.Stocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class StocksService {
 
     @Autowired
     private StocksRepository stocksRepository;
 
-    public Stocks findById(String id) {
-        return fill(
-                stocksRepository
-                        .findById(id)
-                        .map(StocksModel::to)
-                        .orElse(null)
-        );
-    }
-
-    private Stocks fill(Stocks c) {
-        if (c != null) {
-
-        }
-        return c;
+    public List<Stocks> listBy(String idStock, String symbolStock, String nameStock, Date dateTime, BigDecimal valueStock) {
+        return stocksRepository
+                .listBy(idStock, symbolStock, nameStock, dateTime, valueStock).stream()
+                .map(StocksModel::to)
+                .collect(Collectors.toList());
     }
 
 }
